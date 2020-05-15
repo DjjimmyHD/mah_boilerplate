5b1edb7db7c5b5e3d9f3da06d346c991e994709b
jmoney <jmoney@and-the-bank.local>
2020-05-14 20:12:08 -0600

    migrated server into server.js updated scripts in package.json

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 8080;
 
 app.get("/", (req, res, next) => {
   res.send("🐲🐲🐲🐲🐲🐲🐲");
 });
 
-app.listen(port, () => {
-  console.log(`this server is listening to you on ${port} ....?????`);
-});
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,6 @@
+const app = require("./app");
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`this server is listening to you on ${port} ....?????`);
+});
