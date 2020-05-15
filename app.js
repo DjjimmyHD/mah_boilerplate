b65ab55e463595ab22faf1cb7d3c8375f2ecf0bd
jmoney <jmoney@and-the-bank.local>
2020-05-14 20:40:41 -0600

    created config.js and added morgan, specified production developemt

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
+const { NODE_ENV } = require("./config");
 const app = express();
+
+app.use(
+  morgan(NODE_ENV === "production" ? "tiny" : "common", {
+    skip: () => NODE_ENV === "test",
+  })
+);
 
 app.get("/", (req, res, next) => {
   res.send("🐲🐲🐲🐲🐲🐲🐲");
