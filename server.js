9aca416a465705483f508a80f9a0b8b3c6953905
jmoney <jmoney@and-the-bank.local>
2020-05-14 20:28:10 -0600

    added dotenv npm and created .env file for PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
-const port = 8080;
+const port = process.env.PORT;
 
 app.listen(port, () => {
-  console.log(`this server is listening to you on ${port} ....?????`);
+  console.log(`this server is listening to you on ${port}...`);
 });
